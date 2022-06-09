47b0bca89ba148540a386f625b83427155fab396
jonasmachados <jonasmachado.ti@gmail.com>
2022-06-09 13:58:31 -0300

    Created entities Employee

---
+++ springboot-backend/src/main/java/com/Jonas/springbootbackend/model/Employee.java
@@ -0,0 +1,73 @@
+
+package com.Jonas.springbootbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    
+    
+    
+    
+}
---
+++ springboot-backend/src/main/java/com/Jonas/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.Jonas.springbootbackend.repository;
+
+import com.Jonas.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    
+    
+}
