e6871e1748c1f8a4ca797c40451eaa787c49cdd3
jonasmachados <jonasmachado.ti@gmail.com>
2022-06-10 10:59:38 -0300

    Created list employee component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello word!</h1>
-      </header>
+    <div className="container">
+      <ListEmployeeComponent />
     </div>
   );
 }
