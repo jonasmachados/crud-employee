1edd40a978e6721b23780a3065d57aacff4e21c4
jonasmachados <jonasmachado.ti@gmail.com>
2022-06-10 10:01:59 -0300

    Created react frontend project

--- springboot-backend/src/main/java/com/Jonas/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/Jonas/springbootbackend/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api")
 public class EmployeeController {
 
     @Autowired
