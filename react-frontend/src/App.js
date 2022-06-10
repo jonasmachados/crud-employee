106e8369301e56ccb82ba79add61b44b2d504b00
jonasmachados <jonasmachado.ti@gmail.com>
2022-06-10 15:53:56 -0300

    Created Header and Footer

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
-    </div>
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
+    </div >
   );
 }
 
